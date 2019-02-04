410210de4260730561aede25e128965e569930b6
rmpestano <rmpestano@gmail.com>
2019-02-03 22:39:35 -0200

    perf

--- src/main/java/com/github/adminfaces/addon/scaffold/model/FieldConfig.java
+++ src/main/java/com/github/adminfaces/addon/scaffold/model/FieldConfig.java
@@ -1,82 +1,82 @@
 package com.github.adminfaces.addon.scaffold.model;
 
 /**
- * 
+ *
  * @author rafael-pestano
- * 
- *         Represents an entity field configuration to be used on pages
+ *
+ * Represents an entity field configuration to be used on pages
  *
  */
 public class FieldConfig {
 
-	private String name;
-	private Boolean required;
-	private Boolean hidden;
-	private Integer length;
-	private ComponentTypeEnum type;
-	
-	public FieldConfig() {
-	}
+    private String name;
+    private Boolean required;
+    private Boolean hidden;
+    private Integer length;
+    private ComponentTypeEnum type;
 
-	public FieldConfig(String name, Boolean required, Boolean hidden, Integer length, ComponentTypeEnum type) {
-		super();
-		this.name = name;
-		this.required = required;
-		this.hidden = hidden;
-		this.length = length;
-		this.type = type;
-	}
+    public FieldConfig() {
+    }
 
-	public String getName() {
-		return name;
-	}
+    public FieldConfig(String name, Boolean required, Boolean hidden, Integer length, ComponentTypeEnum type) {
+        super();
+        this.name = name;
+        this.required = required;
+        this.hidden = hidden;
+        this.length = length;
+        this.type = type;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Boolean isRequired() {
-		return required;
-	}
-    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean isRequired() {
+        return required;
+    }
+
     public Boolean getRequired() {
-		return required;
-	}
+        return required;
+    }
 
-	public void setRequired(Boolean required) {
-		this.required = required;
-	}
+    public void setRequired(Boolean required) {
+        this.required = required;
+    }
 
-	public Boolean isHidden() {
-		return hidden;
-	}
-    
+    public Boolean isHidden() {
+        return hidden;
+    }
+
     public Boolean getHidden() {
-		return hidden;
-	}
+        return hidden;
+    }
 
-	public void setHidden(Boolean hidden) {
-		this.hidden = hidden;
-	}
+    public void setHidden(Boolean hidden) {
+        this.hidden = hidden;
+    }
 
-	public ComponentTypeEnum getType() {
-		return type;
-	}
+    public ComponentTypeEnum getType() {
+        return type;
+    }
 
-	public void setType(ComponentTypeEnum type) {
-		this.type = type;
-	}
+    public void setType(ComponentTypeEnum type) {
+        this.type = type;
+    }
 
-	public String getTypeLower() {
-		return type.name().toLowerCase();
-	}
+    public String getTypeLower() {
+        return type.name().toLowerCase();
+    }
 
-	public Integer getLength() {
-		return length;
-	}
+    public Integer getLength() {
+        return length;
+    }
 
-	public void setLength(Integer length) {
-		this.length = length;
-	}
+    public void setLength(Integer length) {
+        this.length = length;
+    }
 
 }
