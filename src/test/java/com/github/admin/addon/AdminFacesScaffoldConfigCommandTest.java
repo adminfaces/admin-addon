fa4c8da89839c4e24d6a9a824ffff71415c1c744
rmpestano <rmpestano@gmail.com>
2019-03-05 22:50:35 -0300

    adds entityConfig command test

--- src/test/java/com/github/admin/addon/AdminFacesScaffoldConfigCommandTest.java
+++ src/test/java/com/github/admin/addon/AdminFacesScaffoldConfigCommandTest.java
@@ -49,7 +49,7 @@ public class AdminFacesScaffoldConfigCommandTest {
     @Deployment
     @AddonDependencies
     public static AddonArchive getDeployment() {
-            return ShrinkWrap.create(AddonArchive.class).addBeansXML()
+        return ShrinkWrap.create(AddonArchive.class).addBeansXML()
             .addPackages(true, "org.assertj.core");
     }
 
@@ -84,26 +84,26 @@ public class AdminFacesScaffoldConfigCommandTest {
         shellTest.clearScreen();
         ResourcesFacet resourcesFacet = project.getFacet(ResourcesFacet.class);
         Resource<?> scaffoldDir = resourcesFacet.getResourceDirectory().getChild("scaffold");
-        FileResource<?> talkScaffoldConfig = scaffoldDir.getChild("talk.yml").reify(FileResource.class);
+        FileResource<?> globalScaffoldConfig = scaffoldDir.getChild("global-config.yml").reify(FileResource.class);
         StringBuilder scaffoldConfigCommand = new StringBuilder("adminfaces-scaffold-config --config-file ")
-            .append(talkScaffoldConfig.getFullyQualifiedName()).append(" --input-size 30 --to-one-component-type ")
+            .append(globalScaffoldConfig.getFullyQualifiedName()).append(" --input-size 30 --to-one-component-type ")
             .append(SELECT_ONE_MENU.name()).append(" --to-many-component-type ").append(SELECT_MANY_MENU.name())
             .append(" --datatable-editable --menu-icon \"fa fa-edit\"");
         Result scaffoldConfigResult = shellTest.execute(scaffoldConfigCommand.toString(), 5, TimeUnit.MINUTES);
         if (scaffoldConfigResult instanceof Failed) {
             ((Failed) scaffoldConfigResult).getException().printStackTrace();
         }
-        File globalConfigFile = new File(talkScaffoldConfig.getFullyQualifiedName());
+        File globalConfigFile = new File(globalScaffoldConfig.getFullyQualifiedName());
         assertThat(contentOf(globalConfigFile))
-            .contains("!!com.github.adminfaces.addon.scaffold.model.GlobalConfig"+ NEW_LINE
-                + "datatableEditable: true"+ NEW_LINE
-                + "datatableReflow: true"+ NEW_LINE
+            .contains("!!com.github.adminfaces.addon.scaffold.model.GlobalConfig" + NEW_LINE
+                + "datatableEditable: true" + NEW_LINE
+                + "datatableReflow: true" + NEW_LINE
                 + "dateComponentType: " + CALENDAR.name()
-                        + ""+ NEW_LINE
-                + "inputSize: 30"+ NEW_LINE
-                + "menuIcon: fa fa-edit"+ NEW_LINE
+                + "" + NEW_LINE
+                + "inputSize: 30" + NEW_LINE
+                + "menuIcon: fa fa-edit" + NEW_LINE
                 + "toManyComponentType: " + SELECT_MANY_MENU.name()
-                + ""+ NEW_LINE
+                + "" + NEW_LINE
                 + "toOneComponentType: " + SELECT_ONE_MENU.name()
                 + "");
     }
@@ -113,28 +113,55 @@ public class AdminFacesScaffoldConfigCommandTest {
         shellTest.clearScreen();
         ResourcesFacet resourcesFacet = project.getFacet(ResourcesFacet.class);
         Resource<?> scaffoldDir = resourcesFacet.getResourceDirectory().getChild("scaffold");
-        FileResource<?> globalScaffoldConfig = scaffoldDir.getChild("global-config.yml").reify(FileResource.class);
+        FileResource<?> talkScaffoldConfig = scaffoldDir.getChild("Talk.yml").reify(FileResource.class);
         StringBuilder scaffoldConfigCommand = new StringBuilder("adminfaces-scaffold-config --config-file ")
-                .append(globalScaffoldConfig.getFullyQualifiedName()).append(" --input-size 30 --to-one-component-type ")
-                .append(SELECT_ONE_MENU.name()).append(" --to-many-component-type ").append(SELECT_MANY_MENU.name())
-                .append(" --datatable-editable --menu-icon \"fa fa-edit\"");
+            .append(talkScaffoldConfig.getFullyQualifiedName()).append(" --choice-field-to-change date --type ").append(DATEPICKER.name())
+            .append(" --datatable-editable --menu-icon \"fa fa-edit\"");
         Result scaffoldConfigResult = shellTest.execute(scaffoldConfigCommand.toString(), 5, TimeUnit.MINUTES);
         if (scaffoldConfigResult instanceof Failed) {
             ((Failed) scaffoldConfigResult).getException().printStackTrace();
         }
-        File globalConfigFile = new File(globalScaffoldConfig.getFullyQualifiedName());
-        assertThat(contentOf(globalConfigFile))
-                .contains("!!com.github.adminfaces.addon.scaffold.model.GlobalConfig"+ NEW_LINE
-                        + "datatableEditable: true"+ NEW_LINE
-                        + "datatableReflow: true"+ NEW_LINE
-                        + "dateComponentType: " + CALENDAR.name()
-                        + ""+ NEW_LINE
-                        + "inputSize: 30"+ NEW_LINE
-                        + "menuIcon: fa fa-edit"+ NEW_LINE
-                        + "toManyComponentType: " + SELECT_MANY_MENU.name()
-                        + ""+ NEW_LINE
-                        + "toOneComponentType: " + SELECT_ONE_MENU.name()
-                        + "");
+
+        scaffoldConfigCommand = new StringBuilder("adminfaces-scaffold-config --config-file ")
+            .append(talkScaffoldConfig.getFullyQualifiedName()).append(" --choice-field-to-change description --required ");
+        scaffoldConfigResult = shellTest.execute(scaffoldConfigCommand.toString(), 5, TimeUnit.MINUTES);
+        if (scaffoldConfigResult instanceof Failed) {
+            ((Failed) scaffoldConfigResult).getException().printStackTrace();
+        }
+
+        File talkEntityConfigFile = new File(talkScaffoldConfig.getFullyQualifiedName());
+        assertThat(contentOf(talkEntityConfigFile))
+            .contains("!!com.github.adminfaces.addon.scaffold.model.EntityConfig" + NEW_LINE
+                + "datatableEditable: true" + NEW_LINE
+                + "datatableReflow: true" + NEW_LINE
+                + "displayField: title" + NEW_LINE
+                + "fields:" + NEW_LINE
+                + "- hidden: false" + NEW_LINE
+                + "  length: 100" + NEW_LINE
+                + "  name: id" + NEW_LINE
+                + "  required: true" + NEW_LINE
+                + "  type: INPUT_NUMBER" + NEW_LINE
+                + "- hidden: false" + NEW_LINE
+                + "  length: 100" + NEW_LINE
+                + "  name: version" + NEW_LINE
+                + "  required: false" + NEW_LINE
+                + "  type: INPUT_NUMBER" + NEW_LINE
+                + "- hidden: false" + NEW_LINE
+                + "  length: 100" + NEW_LINE
+                + "  name: title" + NEW_LINE
+                + "  required: true" + NEW_LINE
+                + "  type: INPUT_TEXT" + NEW_LINE
+                + "- hidden: false" + NEW_LINE
+                + "  length: 2000" + NEW_LINE
+                + "  name: description" + NEW_LINE
+                + "  required: true" + NEW_LINE
+                + "  type: TEXT_AREA" + NEW_LINE
+                + "- hidden: false" + NEW_LINE
+                + "  length: 100" + NEW_LINE
+                + "  name: date" + NEW_LINE
+                + "  required: true" + NEW_LINE
+                + "  type: DATEPICKER" + NEW_LINE
+                + "menuIcon: fa fa-circle-o");
     }
 
     private void generateEntities() throws TimeoutException {
